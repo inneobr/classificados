dfe3c379e3190a7b210f79d8f9730054b74d171c
Eduardo Cruz <tecnologiaesolucoespr@gmail.com>
2024-02-10 12:45:40 -0300

    feat: classificados-conf

--- src/main/java/org/inneo/classificados/RunApplication.java
+++ src/main/java/org/inneo/classificados/RunApplication.java
@@ -1,13 +1,19 @@
 package org.inneo.classificados;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@Slf4j
 @SpringBootApplication
 public class RunApplication {
-
+	@Value("${server.port}")	
+	private static String serverPort;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(RunApplication.class, args);
+		log.info("server port: {}", serverPort);
 	}
 
 }
